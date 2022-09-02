097504ac020404d5ea87fcd7cdd229599b16a834
lesly <lesly@DESKTOP-KTHIQA8>
2022-09-02 08:32:11 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_semana2_consola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
